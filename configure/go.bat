4f1937d549a7649006f183273e7b2d798b9d97a7
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2004-11-18 14:32:36 +0000

    An example of "python configure.py [options]" on Windows

---
+++ configure/go.bat
@@ -0,0 +1,3 @@
+REM example for "python configure.py [options]" on Windows
+
+python configure.py -I C:\mystuff\qwtplot3d\include -L C:\mystuff\qwtplot3d\lib --extra-cxxflags=-GR --extra-cxxflags=-GX
