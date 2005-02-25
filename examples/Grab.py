6894290418ff1eda90a7847392857f48a047be76
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2005-02-25 05:26:31 +0000

    A tool to grab the widgets

---
+++ examples/Grab.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# iqt is part of PyQwt
+import iqt
+
+def main():
+    for demo in ['ParametricSurfaceDemo',
+                 'SimplePlot',
+                 ]:
+        result = __import__(demo).make()
+        raw_input("Is the demo looking HAPPY? ")
+        result.save(demo + '.png', 'PNG')
+        result.save(demo + '.pdf', 'PDF')
+
+# main()
+
+if __name__ == '__main__':
+    main()
+
+# Local Variables: ***
+# mode: python ***
+# End: ***
