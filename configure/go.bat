10a99070250f83c7233c42914cf259f7f197cc8f
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2006-02-18 10:33:12 +0000

    Prepare release 0.1.1

--- configure/go.bat
+++ configure/go.bat
@@ -1,6 +1,6 @@
 REM Example for "python configure.py [options]" on Windows
 REM Edit the argument for the -Q option to suit your system
 
-python configure.py -Q C:\mystuff\qwtplot3d
+python configure.py -Q ..\qwtplot3d-0.2.6 -Z ..\zlib-1.2.3
 nmake
 nmake install
